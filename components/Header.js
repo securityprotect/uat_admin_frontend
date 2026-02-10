aeda75ff44ef05b7ec8cfb6a4c81e9f88cabe8fe
ParkPing <105095888+securityprotect@users.noreply.github.com>
2026-02-10 11:24:29 +0530

    Delete components directory

--- components/Header.js
+++
@@ -1,9 +0,0 @@
-import { logout } from "../utils/auth";
-
-export default function Header() {
-  return (
-    <div style={{ padding: 12, background: "#020617", textAlign: "right" }}>
-      <button onClick={logout}>Logout</button>
-    </div>
-  );
-}
