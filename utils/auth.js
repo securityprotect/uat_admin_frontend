178b13e4bf05dee534c14de080a6669a206d307c
ParkPing <105095888+securityprotect@users.noreply.github.com>
2026-02-10 11:24:47 +0530

    Delete utils directory

--- utils/auth.js
+++
@@ -1,9 +0,0 @@
-export function isLoggedIn() {
-  if (typeof window === "undefined") return false;
-  return !!localStorage.getItem("admin_token");
-}
-
-export function logout() {
-  localStorage.removeItem("admin_token");
-  window.location.href = "/login";
-}
